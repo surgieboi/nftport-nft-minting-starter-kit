95e3b923614d26c19e0a7802a92c11598d599fe0
Sergio Villasenor <42443963+surgieboi@users.noreply.github.com>
2022-10-25 22:50:05 +0300

    update github url

--- components/header/header.js
+++ components/header/header.js
@@ -6,7 +6,7 @@ export default function Header() {
         <>
             <nav className="fixed w-full flex items-center justify-between p-4 bg-slate-100 border-b border-slate-200">
                 <Logo />
-                <a className="p-4 hover:text-white hover:bg-slate-800 border border-slate-200 hover:border-slate-800 rounded-2xl" href="https://github.com/surgieboi/nftport-creator-minting-starter-kit" target="_blank">
+                <a className="p-4 hover:text-white hover:bg-slate-800 border border-slate-200 hover:border-slate-800 rounded-2xl" href="https://github.com/surgieboi/nftport-nft-minting-starter-kit" target="_blank">
                     View on Github
                 </a>
             </nav>
